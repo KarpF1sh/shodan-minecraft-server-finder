843db0004e09d93c7b3bb6ba1c0db227aed727f5
Vilho Mäkeläinen <vilho.makelainen@tuni.fi>
2023-01-29 23:49:54 +0200

    Cleaned up some things

--- minecraft_tool_standalone/Properties/Resources.Designer.cs
+++ minecraft_tool_standalone/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace minecraft_tool_standalone.Properties {
+namespace McTool.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace minecraft_tool_standalone.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("minecraft_tool_standalone.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("McTool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
